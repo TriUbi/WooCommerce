35470dfaf4a62af104af9753f74cfaa497dbceaa
TriUbi <triubilla@gmail.com>
2024-09-11 11:31:20 +0200

    style och funktioner test

--- my-theme/functions.php
+++ my-theme/functions.php
@@ -1,6 +1,5 @@
 <?php
-function enqueue_custom_styles() {
-    wp_enqueue_style('custom-styles', get_stylesheet_directory_uri() . '/styles.css');
+add_action( 'wp_enqueue_scripts', 'storefront_child_enqueue_styles' );
+function storefront_child_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
-add_action('wp_enqueue_scripts', 'enqueue_custom_styles');
-
